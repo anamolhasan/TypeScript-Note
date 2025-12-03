f20742313862204002ab00eaaae84cf20fc34758
anamolhasan <mdanamolhasan1998@gmail.com>
2025-12-03 08:09:33 +0600

    class 4

---
+++ src/class-04.ts
@@ -0,0 +1,128 @@
+// =========================================================
+// ✔️ TypeScript এর প্রধান টাইপগুলো 
+// =========================================================
+
+/*=========================================================
+1️⃣ Primitive Types
+----------------------------------------------------------
+ string, number, boolean, null, undefined, symbol, bigint
+*/
+
+/**=================== Example: String ======================= */
+let a = 'Hello';
+a = a + 'World'  // string concatenation
+console.log(a)   // Hello world
+
+/** ================ Example: Object Reference ===================== */
+let obj1 = {name:'Anam'}
+let obj2 = obj1
+obj2.name = 'Hasan'; // reference update
+console.log(obj2.name)  // Hansan
+
+/** ============= Example: Function Type ================== */
+type Func1 = (value:number) => number;
+
+const func1: Func1 = (value) => {
+    return value
+}
+
+const hello = func1(54)
+console.log(hello); // 54
+
+/*=======================================================
+2️⃣ Literal Type
+---------------------------------------------------------
+Specific value দিয়ে টাইপ তৈরি করা যায়
+*/
+type L = "Hello" | 'anam'
+const literal: L = 'anam'
+
+/*=========================================================
+3️⃣ Optional Property
+Object property optional হলে ? দিয়ে ব্যবহার করা হয়
+*/
+type Optional = {
+    a: number;
+    b?:"i'am";
+    c:'anam'
+}
+
+const optional: Optional = {
+    a:232,
+    b:"i'am",
+    c:'anam'
+}
+
+/*==========================================================
+4️⃣ Union Type
+------------------------------------------------------------
+একাধিক টাইপের মধ্যে যেকোন একটি হতে পারে
+ */
+let direction: 'up' | 'down';
+direction = 'down'
+
+/*==========================================================
+5️⃣ Array & Spread Operator
+ -----------------------------------------------------------
+ Rest/Spread দিয়ে array merge করা যায় 
+ */
+type Arr = number []
+const arr: Arr = [3, 3]
+
+const student2 = [1,2,3,4,56,7]
+const student3 = [5,6,7,8]
+
+student2.push(...student3)  //spread operator
+console.log(student2)   // [1,2,3,4,56,7,5,6,7,8]
+
+const user1: string[] = ['Anamol', 'Hasan', 'shakil']
+const user2: string[] = ['Fatema', 'Sultana', 'Aisha','A']
+
+const res = [...user1, user2] // merge array
+console.log(res)
+
+
+/*=========================================================
+6️⃣ Nested Array Destructuring
+-----------------------------------------------------------
+Array এর মধ্যে array হলে destructuring
+ */
+const arr2 = [21, 32, 43, [12, 22]]
+const [, , , nested] = arr2
+console.log(nested) //[12, 22]
+
+const data: [number,[number, number], [number, [number, number]]] =[
+    1,
+    [2,3],
+    [4,[5,6]]
+]
+const [x,[y,z],[pv,[q,r]]] = data
+console.log(x, y, z) //1, 2, 3
+console.log(pv,q,r) // 4, 5, 6
+
+/*===========================================================
+7️⃣ Union (String | Number)
+-------------------------------------------------------------
+Variable multiple type নিতে পারে
+ */
+let value: string | number
+value = 10;
+value = 'Anam'
+console.log(value)
+
+
+/*============================================================
+8️⃣ Intersection Type
+--------------------------------------------------------------
+একাধিক টাইপ একত্রিত করে নতুন টাইপ বানানো */
+
+type Person = {
+    name:string;
+} & {
+    age:number
+}
+
+let p: Person = {
+    age:4,
+    name:''
+}
